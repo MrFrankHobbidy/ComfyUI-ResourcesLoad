8fcdaaf9ee5f6cd7b434496f29d6b25db5253ba8
MrFrankHobbidy <57764194+MrFrankHobbidy@users.noreply.github.com>
2026-03-30 16:11:22 +0800

    Add files via upload

--- ResourcesLoad.py
+++ ResourcesLoad.py
@@ -1,11 +1,14 @@
 import os
+import base64
 import folder_paths
 import numpy as np
+import node_helpers
+import torch
 
 from PIL import Image, ImageOps
+from io import BytesIO
 
-import torch
-import node_helpers
+import re
 
 class AnyType(str):
     def __eq__(self, _) -> bool:
@@ -32,7 +35,7 @@ class Rload:
     RETURN_TYPES = (any, )
     RETURN_NAMES = ("output", )
     FUNCTION = "load"
-    CATEGORY = "ResourcesSL"
+    CATEGORY = "ResourcesLoad"
 
     # def load(self, npy, anything=None):
         # npy_path = folder_paths.get_annotated_filepath(npy)
@@ -46,13 +49,38 @@ class Rload:
         npy_path = os.path.join(full_output_folder, file)
 
         npyg = np.load(npy_path, allow_pickle=True)
-        # print(f"{npyg}")
         try:
             anything = torch.from_numpy(npyg[0])[None,]
         except Exception as e:
             anything = npyg
             # print(f"{e}")
-        # print(f"{anything}")
+        return (anything, )
+
+class RloadImage:
+    @classmethod
+    def INPUT_TYPES(s):
+        input_dir = folder_paths.get_input_directory()
+        files = [f for f in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, f))]
+        files = folder_paths.filter_files_content_types(files, ["image"])
+        return {"required":
+                    {"image": (sorted(files), {"image_upload": True})},
+                }
+
+    RETURN_TYPES = (any, )
+    RETURN_NAMES = ("output", )
+    FUNCTION = "load_image"
+    CATEGORY = "ResourcesLoad"
+
+    def load_image(self, image):
+        image_path = folder_paths.get_annotated_filepath(image)
+        img = node_helpers.pillow(Image.open, image_path)
+        npyb = base64.b64decode(re.sub(r"\"b\'(.*?)\'\"", r"\1", img.info.get("ResourcesSave")))
+        npyg = np.load(BytesIO(npyb), allow_pickle=True)
+        try:
+            anything = torch.from_numpy(npyg[0])[None,]
+        except Exception as e:
+            anything = npyg
+            # print(f"{e}")
         return (anything, )
 
 class RloadImageC:
@@ -67,15 +95,14 @@ class RloadImageC:
     RETURN_TYPES = ("IMAGE", )
     RETURN_NAMES = ("images", )
     FUNCTION = "imagecl"
-    CATEGORY = "ResourcesSL"
+    CATEGORY = "ResourcesLoad"
 
     def imagecl(self, anything=None):
         imageg = []
         for imgb in anything:
             img = Image.open(imgb)
             img = node_helpers.pillow(ImageOps.exif_transpose, img)
-            # print(f"{img.mode}")
-            if img.mode == 'I':
+            if img.mode == "I":
                 img = img.point(lambda img: img * (1 / 255))
             imgd = img.convert("RGB")
             i = np.array(imgd).astype(np.float32) / 255.0
@@ -89,9 +116,11 @@ class RloadImageC:
 
 NODE_CLASS_MAPPINGS = {
     "Rload": Rload,
+    "RloadImage": RloadImage,
     "RloadImageC": RloadImageC,
 }
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "Rload": "Rload",
+    "Rload": "RloadNpy",
+    "RloadImage": "RloadImage",
     "RloadImageC": "RloadImageC",
 }
